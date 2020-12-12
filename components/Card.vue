03f5d43561727b3882f63a30d1d6697d60829ed5
Sedon Attila <h985176@stud.u-szeged.hu>
2020-12-12 15:29:11 +0100

    First working draft, created: Card, CardList, and index with only Nuxt.js

---
+++ components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img :src=card.img.src :alt=card.img.alt :title=card.img.title>
+    <img :src=card.thumbnail.src :alt=card.thumbnail.alt :title=card.thumbnail.title>
+    <h3>{{card.title}}</h3>
+    <h4>{{card.subTitle}}</h4>
+    <div v-html="card.content" />
+    <div v-html="card.footer" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {card:         {
+      img: {
+        alt: String,
+        src: String,
+        title: String,
+      },
+      title: String,
+      subTitle: String,
+      content: String,
+      footer: String,
+      thumbnail: {
+        alt: String,
+        src: String,
+        title: String,
+      }
+    }}
+}
+</script>
---
+++ components/CardList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for="card in cards">
+      <Card v-bind:card="card"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './Card';
+
+export default {
+  name: "CardList",
+  components: {
+    Card
+  },
+  props: ["cards"]
+}
+</script>
